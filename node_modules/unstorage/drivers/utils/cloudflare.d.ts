2945011e81d6472d286a6c90d06257320f3e6eec
Yulian Bedoya <ybedoyab@unal.edu.co>
2024-07-03 15:21:32 -0500

    commit

--- node_modules/unstorage/drivers/utils/cloudflare.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="@cloudflare/workers-types" />
-export declare function getBinding(binding: KVNamespace | R2Bucket | string): KVNamespace<string> | R2Bucket;
-export declare function getKVBinding(binding?: KVNamespace | string): KVNamespace<string>;
-export declare function getR2Binding(binding?: R2Bucket | string): R2Bucket;
